9cee41a07ed9ce14180a42924e943b4f58b407a7
Alex Henderson <alex_henderson@bradycorp.com>
2019-01-10 12:30:11 -0500

    Added another powershell script

---
+++ Scripts/powershell_scripts/restartserviceifstopped.ps1
@@ -0,0 +1,12 @@
+#This will need to be run as admin in order for it to interact with services.
+$Service = Get-Service -Name 'name of service'
+while ($Service.Status -ne 'Running')
+{
+    Start-Service $Service
+    Start-Sleep -Seconds 60
+    $Service.Refresh()
+    if ($Service.Status -eq 'Running')
+    {
+        Write-Host $Service 'is now running'
+    }
+}
