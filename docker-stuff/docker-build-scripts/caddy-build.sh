d54a679fd37f51e3d4cce9fb5c5a561907a8de47
Alex Henderson <alex_henderson@bradycorp.com>
2019-01-28 14:58:59 -0500

    Updated caddy-build.sh for future alexandzors/caddy:cf update

--- docker-stuff/docker-build-scripts/caddy-build.sh
+++ docker-stuff/docker-build-scripts/caddy-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Use this shell script to build a custom docker image for Caddy server based on abiosoft/caddy with no telemetery stats
 #$PLUGINS should be a comma seperated list.
-$PLUGINS=filemanager,git,cloudflare,geoip,realip,nobots
+$PLUGINS=filebrowser,git,cloudflare,geoip,realip,nobots,reauth,login,webdav
 
 docker build --build-arg -f Dockerfile-no-stats plugins=$PLUGINS github.com/abiosoft/caddy-docker.git
